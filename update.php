d8a038d19b35a3028ed365ae6bc246e9b2649498
fiddl3stick <126037702+fiddl3stick@users.noreply.github.com>
2024-02-28 13:50:56 +0800

    Add files via upload

---
+++ update.php
@@ -0,0 +1,111 @@
+<?php session_start();
+include ("config.php");
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+          crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h1 class="text-center">Update Student Data</h1>
+<div class="container mt-4">
+    <div class="row justify-content-center">
+        <div class="col-lg-9">
+
+        <?php
+        if(isset($_GET['id']))
+        {
+            $id = $_GET['id'];
+            $users = "SELECT * FROM `user` WHERE `id` = '$id'";
+            $users_run = mysqli_query($con, $users);
+
+            if(mysqli_num_rows($users_run) > 0)
+            {
+                foreach($users_run as $user)
+                {
+                ?>
+
+            <form action="process.php" method="POST">
+
+            <input type="hidden" name="id" value="<?=$user['id'];?>">
+
+                <div class="row">
+                    <div class="col-md-12 mb-3">
+                        <label for="studentId" class="form-label">Title</label>
+                        <input type="text" class="form-control" id="studentId" value="<?=$user['student_id'];?>" name="student_id" required>
+                    </div>
+
+                    <div class="col-md-4 mb-3">
+                        <label for="fname" class="form-label">Description</label>
+                        <textarea rows=5  placeholder="whattaya thinkin'" class="form-control" id="fname" value="<?=$user['fname'];?>" name="fname"required></textarea>
+                    </div>
+
+                    <div class="col-md-4 mb-3">
+                        <label for="email" class="form-label">Priority</label>
+                         <select class="form-select mb-2" name="email" class="form-control">
+                                <option value="" disabled selected>Select priority</option>
+                                <option value="Low">Low</option>
+                                <option value="Medium">Medium</option>
+                                <option value="High">High</option>
+                            </select>
+                    </div>
+
+                    <div class="col-md-4 mb-3">
+                        <label for="birthday" class="form-label">Due_date</label>
+                        <input type="date" class="form-control" id="birthday" value="<?=$user['birthday'];?>" name="birthday" required>
+                    </div>
+
+                    <div class="col-md-12 mb-3 text-center">
+                        <button type="submit" class="btn btn-primary"  style="float: right;" name="update">Update</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <?php
+                }
+            }
+            else
+            {
+                ?>
+                <h4>No Record Found!</h4>
+                <?php
+            }
+        }
+?>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+        crossorigin="anonymous"></script>
+
+
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<?php
+if (isset($_SESSION['status']) && $_SESSION['status_code'] != '' )
+{
+    ?>
+    <script>
+        swal({
+            title: "<?php echo $_SESSION['status']; ?>",
+            icon: "<?php echo $_SESSION['status_code']; ?>",
+        });
+    </script>
+    <?php
+    unset($_SESSION['status']);
+    unset($_SESSION['status_code']);
+}
+?>
+</body>
+</html>
